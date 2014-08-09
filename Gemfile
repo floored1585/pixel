975ce8685651d506c08823e62ef223f7cebd17df
Ian Clark <ian.clark@dreamhost.com>
2014-08-08 17:17:14 -0700

    initial commit - shit's broken yo

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=sinatra
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'pg'
+
---
+++ lib/core_ext/string.rb
@@ -0,0 +1,8 @@
+class String
+  def is_i?
+    !!(self =~ /^[\d]+$/)
+  end
+  def to_i_if_numeric
+    self.is_i? ? self.to_i : self
+  end
+end

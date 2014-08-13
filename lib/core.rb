5f1276d8c05e9a480c893df502f1cf086286ac92
Ian Clark <ian@f85.net>
2014-08-13 16:03:17 -0700

    Fixing a lot of Sequel stuff, refactoring, renaming

--- lib/core.rb
+++ lib/core.rb
@@ -3,73 +3,56 @@
 require_relative 'core_ext/string.rb'
 
 module Pixel
-  def interfaces_down(settings,db_handle,opts = {})
-    query = "( if_alias LIKE 'sub%' OR if_alias LIKE 'bb%' ) AND if_oper_status != 1"
+  def get_ints_down(settings, db)
+    rows = db[:current].filter(Sequel.like(:if_alias, 'sub%') | Sequel.like(:if_alias, 'bb%'))
+    rows = rows.exclude(:device => 'test')
+    rows = rows.exclude(:if_oper_status => 1)
 
-    interfaces = _return_interfaces(db_handle,query)
-    (devices,name_to_index) = _device_map(settings,interfaces)
-    metadata = _device_metadata(settings,devices,name_to_index,opts)
+    (devices, name_to_index) = _device_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
 
-    metadata.each do |device,int|
-      int.delete_if do |index,oids|
-        oids[:my_parent] && int[oids[:my_parent]]
-      end 
+    # Delete the interface from the hash if its parent is present, to reduce clutter
+    devices.each do |device,int|
+      int.delete_if { |index,oids| oids[:my_parent] && int[oids[:my_parent]] }
     end
-    return metadata
+    return devices
   end
 
   # done
-  def interfaces_saturated(settings,db_handle,opts = {})
-    query = "( bps_in_util > 90 OR bps_out_util > 90 )"
-    query_option = "limit"
+  def get_ints_saturated(settings, db)
+    rows = db[:current].filter{ (bps_in_util > 90) | (bps_out_util > 90) }
+    rows = rows.exclude(:device => 'test')
 
-    interfaces = _return_interfaces(db_handle,query,query_option)
-    (devices,name_to_index) = _device_map(settings,interfaces)
-    metadata = _device_metadata(settings,devices,name_to_index,opts)
-    return metadata
+    (devices, name_to_index) = _device_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
   end
 
-  def interfaces_discarded(settings,db_handle,opts = {})
-    query = "discards_out > 9 AND if_alias NOT LIKE 'sub%'"
-    query_option = "orderby"
+  def get_ints_discarding(settings, db)
+    rows = db[:current].filter{Sequel.&(discards_out > 9, ~Sequel.like(:if_alias, 'sub%'))}
+    rows = rows.exclude(:device => 'test')
+    rows = rows.order(:discards_out).reverse.limit(10)
 
-    interfaces = _return_interfaces(db_handle,query,query_option)
-    (devices,name_to_index) = _device_map(settings,interfaces)
-    metadata = _device_metadata(settings,devices,name_to_index,opts)
-    return metadata
+    (devices, name_to_index) = _device_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
   end
 
-  def device_interfaces(settings,db_handle,opts = {})
-    device = opts
-    query = "device = '#{device}'"
+  def get_ints_device(settings, db, device)
+    rows = db[:current].filter(:device => device)
 
-    interfaces = _return_interfaces(db_handle,query)
-    (devices,name_to_index) = _device_map(settings,interfaces)
-    metadata = _device_metadata(settings,devices,name_to_index,{:device => device})
-    return metadata
+    (devices, name_to_index) = _device_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
   end
 
-  # totally aware _return_interfaces is ugly
-  # the abstraction with Postgres is weird and may need to
-  # be refactored to better encompass options like order by and limit
-  def _return_interfaces(db_handle,query, option= {})
-    current_table = db_handle[:current]
-    if option.eql?("oderby")
-      dataset = current_table.where(query).exclude(:device => 'test').order(Sequel.desc(:discards_out)).limit(10).all
-    elsif option.eql?("limit")
-      dataset = current_table.where(query).exclude(:device => 'test').limit(10).all
-    else
-      dataset = current_table.where(query).exclude(:device => 'test').all
-    end
-    return dataset
-  end
-
-  def _device_map(settings,interfaces)
+  def _device_map(rows)
     devices = {}
     name_to_index = {}
 
     # If params were passed, use exec_params.  Otherwise just exec.
-    interfaces.each do |row|
+    columns = rows.columns
+    rows.each do |row|
 
       if_index = row[:if_index]
       device = row[:device]
@@ -78,18 +61,17 @@ module Pixel
       name_to_index[device] ||= {}
       devices[device][if_index] = {}
 
-      settings['pg_attrs'].each do |attr|
-        attr = attr.to_sym
-        devices[device][if_index][attr] = row[attr].to_s.to_i_if_numeric
-        devices[device][if_index][attr] = nil if devices[device][if_index][attr].to_s.empty?
+      columns.each do |column|
+        devices[device][if_index][column] = row[column].to_s.to_i_if_numeric
+        devices[device][if_index][column] = nil if devices[device][if_index][column].to_s.empty?
       end
       name_to_index[device][row[:if_name].downcase] = if_index
     end
 
-    return devices,name_to_index
+    return devices, name_to_index
   end
 
-  def _device_metadata(settings,devices,name_to_index,opts={})
+  def _fill_metadata!(devices, settings, name_to_index)
     devices.each do |device,interfaces|
       interfaces.each do |index,oids|
         # Populate 'neighbor' value
@@ -125,8 +107,5 @@ module Pixel
         oids[:if_oper_status] == 1 ? oids[:link_up] = true : oids[:link_up] = false
       end
     end
-
-    return devices[opts[:device]] if opts[:device]
-    return devices
   end
 end
--- pixel.rb
+++ pixel.rb
@@ -8,15 +8,13 @@ require 'rack/coffee'
 # Load the modules
 Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |f| require(f) }
 
-set :environment, :development
-
 include Configfile
 include SQ
 include Pixel
 include Helper
 
 settings = Configfile.retrieve
-db_handle = SQ.initiate
+db = SQ.initiate
 
 # COFFEESCRIPT PLZ
 use Rack::Coffee, root: 'public', urls: '/javascripts'
@@ -25,17 +23,17 @@ get '/' do
   # Start timer
   beginning = Time.now
 
-  ints_saturated = interfaces_saturated(settings,db_handle)
-  ints_discarded = interfaces_discarded(settings,db_handle)
-  ints_down = interfaces_down(settings,db_handle)
+  ints_saturated = get_ints_saturated(settings, db)
+  ints_discarding = get_ints_discarding(settings, db)
+  ints_down = get_ints_down(settings, db)
 
   db_elapsed = '%.2f' % (Time.now - beginning)
 
   erb :dashboard, :locals => {
     :title => 'Dashboard!',
     :settings => settings,
-    :ints_dis => ints_discarded,
-    :ints_sat => ints_saturated,
+    :ints_discarding => ints_discarding,
+    :ints_saturated => ints_saturated,
     :ints_down => ints_down,
     :db_elapsed => db_elapsed,
   }
@@ -46,11 +44,12 @@ get '/device/search' do
   redirect to(target.empty? ? '/' : "/device/#{target}")
 end
 
+
 get '/device/:device' do |device|
   # Start timer
   beginning = Time.now
 
-  interfaces = device_interfaces(settings,db_handle,device)
+  interfaces = get_ints_device(settings, db, device)
 
   # How long did it take us to query the database
   db_elapsed = '%.2f' % (Time.now - beginning)
@@ -58,7 +57,8 @@ get '/device/:device' do |device|
   erb :device, :locals => {
     :settings => settings,
     :device => device,
-    :interfaces => interfaces,
+    :interfaces => interfaces[device],
     :db_elapsed => db_elapsed,
   }
 end
+

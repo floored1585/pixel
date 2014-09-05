cefb2e79611a1bb0b19764d9afd947c157b138a9
Ian Clark <ian@f85.net>
2014-09-04 17:14:22 -0700

    Reorganizing lib/core.rb

--- lib/core.rb
+++ lib/core.rb
@@ -2,17 +2,45 @@
 
 module Core
 
-  def add_devices(settings, db, devices)
-    db.disconnect
-    devices.each do |device, ip|
-      existing = db[:device].where(:device => device)
-      if existing.update(:ip => ip) != 1
-        $LOG.info("CORE: Adding new device: #{device}")
-        db[:device].insert(:device => device, :ip => ip)
-      end
+  def get_ints_down(settings, db)
+    rows = db[:current].filter(Sequel.like(:if_alias, 'sub%') | Sequel.like(:if_alias, 'bb%'))
+    rows = rows.exclude(:if_oper_status => 1)
+
+    (devices, name_to_index) = _interface_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+
+    # Delete the interface from the hash if its parent is present, to reduce clutter
+    devices.each do |device,int|
+      int.delete_if { |index,oids| oids[:my_parent] && int[oids[:my_parent]] }
     end
-    # need error detection
-    return true
+    return devices
+  end
+
+  def get_ints_saturated(settings, db)
+    rows = db[:current].filter{ (bps_in_util > 90) | (bps_out_util > 90) }
+
+    (devices, name_to_index) = _interface_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
+  end
+
+  def get_ints_discarding(settings, db)
+    rows = db[:current].filter{Sequel.&(discards_out > 9, ~Sequel.like(:if_alias, 'sub%'))}
+    rows = rows.order(:discards_out).reverse.limit(10)
+
+    (devices, name_to_index) = _interface_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
+  end
+
+  def get_ints_device(settings, db, device)
+    rows = db[:current]
+    # Filter If a device was specified, otherwise return all
+    rows = rows.filter(:device => device) if device
+
+    (devices, name_to_index) = _interface_map(rows)
+    _fill_metadata!(devices, settings, name_to_index)
+    return devices
   end
 
   def get_devices_poller(settings, db, count, poller_name)
@@ -47,46 +75,68 @@ module Core
     return devices
   end
 
-  def get_ints_down(settings, db)
-    rows = db[:current].filter(Sequel.like(:if_alias, 'sub%') | Sequel.like(:if_alias, 'bb%'))
-    rows = rows.exclude(:if_oper_status => 1)
+  def post_devices(settings, db, devices)
+    _validate_devices_post!(devices)
 
-    (devices, name_to_index) = _interface_map(rows)
-    _fill_metadata!(devices, settings, name_to_index)
+    devices.each do |device, data|
 
-    # Delete the interface from the hash if its parent is present, to reduce clutter
-    devices.each do |device,int|
-      int.delete_if { |index,oids| oids[:my_parent] && int[oids[:my_parent]] }
+      metadata = data[:metadata]
+      interfaces = data[:interfaces]
+
+      $LOG.info("CORE: Received data for #{device} from #{data[:metadata][:worker]}")
+
+      data[:interfaces].each do |if_index, oids|
+        #$LOG.warn("Device: #{device}  Interface: #{if_index}\n  OIDs: #{oids}")
+        # Try updating, and if we don't affect a row, insert instead
+        existing = db[:current].where(:device => oids[:device], :if_index => if_index)
+        if existing.update(oids) != 1
+          db[:current].insert(oids)
+        end
+      end
+
+      # Update the device metadata
+      next_poll = Time.now.to_i + 100
+      db[:device].where(:device => device).update(
+        :currently_polling => 0,
+        :worker => nil,
+        :last_poll_duration => metadata[:last_poll_duration],
+        :last_poll_result => metadata[:last_poll_result],
+        :last_poll_text => metadata[:last_poll_text],
+        :next_poll => next_poll,
+      )
     end
-    return devices
+    return true
   end
 
-  # done
-  def get_ints_saturated(settings, db)
-    rows = db[:current].filter{ (bps_in_util > 90) | (bps_out_util > 90) }
+  def populate_device_table(settings, db)
+    db.disconnect
+    devices = {}
 
-    (devices, name_to_index) = _interface_map(rows)
-    _fill_metadata!(devices, settings, name_to_index)
-    return devices
+    # Load from file
+    if settings['device_source']['type'] = 'file'
+      device_file = settings['device_source']['file_path']
+      if File.exists?(device_file)
+        devices = YAML.load_file(File.join(APP_ROOT, device_file))
+      else
+        $LOG.error("CORE: Error populating devices from file: File not found: #{device_file}")
+      end
+      $LOG.info("CORE: Importing #{devices.size} devices from file: #{device_file}")
+    end
+
+    API.post('core', '/v1/devices/add', devices, 'CORE', 'new devices')
   end
 
-  def get_ints_discarding(settings, db)
-    rows = db[:current].filter{Sequel.&(discards_out > 9, ~Sequel.like(:if_alias, 'sub%'))}
-    rows = rows.order(:discards_out).reverse.limit(10)
-
-    (devices, name_to_index) = _interface_map(rows)
-    _fill_metadata!(devices, settings, name_to_index)
-    return devices
-  end
-
-  def get_ints_device(settings, db, device)
-    rows = db[:current]
-    # Filter If a device was specified, otherwise return all
-    rows = rows.filter(:device => device) if device
-
-    (devices, name_to_index) = _interface_map(rows)
-    _fill_metadata!(devices, settings, name_to_index)
-    return devices
+  def add_devices(settings, db, devices)
+    db.disconnect
+    devices.each do |device, ip|
+      existing = db[:device].where(:device => device)
+      if existing.update(:ip => ip) != 1
+        $LOG.info("CORE: Adding new device: #{device}")
+        db[:device].insert(:device => device, :ip => ip)
+      end
+    end
+    # need error detection
+    return true
   end
 
   def _validate_devices_post!(devices)
@@ -126,38 +176,6 @@ module Core
       devices = {}
     end
     return devices
-  end
-
-  def post_devices(settings, db, devices)
-    _validate_devices_post!(devices)
-
-    devices.each do |device, data|
-
-      metadata = data[:metadata]
-      interfaces = data[:interfaces]
-
-      $LOG.info("CORE: Received data for #{device} from #{data[:metadata][:worker]}")
-
-      data[:interfaces].each do |if_index, oids|
-        # Try updating, and if we don't affect a row, insert instead
-        existing = db[:current].where(:device => oids[:device], :if_index => if_index)
-        if existing.update(oids) != 1
-          db[:current].insert(oids)
-        end
-      end
-
-      # Update the device metadata
-      next_poll = Time.now.to_i + 100
-      db[:device].where(:device => device).update(
-        :currently_polling => 0,
-        :worker => nil,
-        :last_poll_duration => metadata[:last_poll_duration],
-        :last_poll_result => metadata[:last_poll_result],
-        :last_poll_text => metadata[:last_poll_text],
-        :next_poll => next_poll,
-      )
-    end
-    return true
   end
 
   def _interface_map(rows)
@@ -215,24 +233,6 @@ module Core
         oids[:if_oper_status] == 1 ? oids[:link_up] = true : oids[:link_up] = false
       end
     end
-  end
-
-  def populate_device_table(settings, db)
-    db.disconnect
-    devices = {}
-
-    # Load from file
-    if settings['device_source']['type'] = 'file'
-      device_file = settings['device_source']['file_path']
-      if File.exists?(device_file)
-        devices = YAML.load_file(File.join(APP_ROOT, device_file))
-      else
-        $LOG.error("CORE: Error populating devices from file: No device file found: #{device_file}")
-      end
-      $LOG.info("CORE: Importing #{devices.size} devices from file: #{device_file}")
-    end
-
-    API.post('core', '/v1/devices/add', devices)
   end
 
 end

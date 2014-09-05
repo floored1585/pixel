fde1475e70578e0dd5a9e1bb27d1925d9a86dfb1
Ian Clark <ian@f85.net>
2014-09-04 17:15:11 -0700

    Major refactor of poller and API module

--- lib/api.rb
+++ lib/api.rb
@@ -4,35 +4,43 @@ module API
 
   @settings = Configfile.retrieve
 
-  def self.get(component, request)
-    begin
-      uri = URI('http://' + @settings['components'][component] + request)
-      request = Net::HTTP::Get.new(uri)
-      response = Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
-      JSON.parse(response.body) if response.code == '200'
-    rescue Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET,
-      Errno::ECONNREFUSED, EOFError, Net::HTTPBadResponse,
-      Net::HTTPHeaderSyntaxError, Net::ProtocolError
-      # HTTP request failed
-      $LOG.error("API: API call GET failed: #{uri}")
-      return false
-    end
+  def self.get(dst_component, request, src_component, task)
+    uri = URI('http://' + @settings['components'][dst_component] + request)
+    request = Net::HTTP::Get.new(uri)
+    response = _execute_request(uri, request, 'GET', src_component, task)
+    response ? JSON.parse(response.body) : false
   end
 
-  def self.post(component, request, rawdata)
-    begin
-      unless rawdata.empty?
-        uri = URI('http://' + @settings['components'][component] + request)
-        request = Net::HTTP::Post.new(uri, {'Content-Type' => 'application/json'})
-        request.body = JSON.generate(rawdata)
-        response = Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
-      end
+  def self.post(dst_component, request, rawdata, src_component, task)
+    return false if rawdata.empty?
+    uri = URI('http://' + @settings['components'][dst_component] + request)
+    request = Net::HTTP::Post.new(uri, {'Content-Type' => 'application/json'})
+    request.body = JSON.generate(rawdata)
+    _execute_request(uri, request, 'POST', src_component, task)
+  end
+
+  def self._execute_request(uri, request, req_type, src_component, task, retry_count=0)
+    retry_limit = @settings['api']['retry_limit'] || 5
+    retry_delay = @settings['api']['retry_delay'] || 5
+    base_log = "#{src_component}: API request to #{req_type} #{task} failed: #{uri}."
+
+    begin # Attempt the connection
+      Net::HTTP.start(uri.host, uri.port) { |http| http.request(request) }
     rescue Timeout::Error, Errno::ETIMEDOUT, Errno::EINVAL, Errno::ECONNRESET,
       Errno::ECONNREFUSED, EOFError, Net::HTTPBadResponse,
       Net::HTTPHeaderSyntaxError, Net::ProtocolError
-      # HTTP request failed
-      $LOG.error("API: API call POST failed: #{uri}")
-      return false
+      # The request failed; Retry if allowed
+      if retry_count <= retry_limit
+        retry_count += 1
+        retry_log = "Retry ##{retry_count} (limit: #{retry_limit}) in #{retry_delay} seconds."
+        $LOG.error("#{base_log}\n  #{retry_log}")
+        sleep retry_delay
+        _execute_request(uri, request, req_type, src_component, task, retry_count)
+      else
+        $LOG.error("#{base_log}\n  Retry limit (#{retry_limit}) exceeded; Aborting.")
+        return false
+      end
+
     end
   end
 
--- lib/poller.rb
+++ lib/poller.rb
@@ -9,214 +9,190 @@ require_relative 'core_ext/hash'
 module Poller
 
   def self.check_for_work(settings, db)
-    concurrency = settings['poller']['concurrency']
-    hostname = Socket.gethostname
-    request = "/v1/devices/fetch_poll?count=#{concurrency}&hostname=#{hostname}"
+    poller_cfg = _poller_cfg(settings)
+    concurrency = poller_cfg[:concurrency]
+    request = "/v1/devices/fetch_poll?count=#{concurrency}&hostname=#{Socket.gethostname}"
 
-    devices = API.get('core', request)
-    if devices
-      devices.each { |device, attributes| _poll(settings, device, attributes['ip']) }
+    if devices = API.get('core', request, 'POLLER', 'devices to poll')
+      devices.each { |device, attributes| _poll(poller_cfg, device, attributes['ip']) }
       return 200 # Doesn't do any error checking here
     else # HTTP request failed
-      $LOG.error("POLLER: HTTP request to check for work failed: #{request}")
       return 500
     end
   end
 
-  def self._poll(settings, device, ip)
-    # Convert poller settings into hash with symbols as keys
-    poller_cfg = settings['poller'].dup || {}
-    poller_cfg.symbolize!
+  def self._poll(poller_cfg, device, ip)
+    pid = fork do
+      start_time = Time.now
+      if_table = {}
+      metadata = { :worker => Socket.gethostname }
 
-    beginning = Time.now
+      # get SNMP data from the device
+      begin
+        if_table = _query_device(ip, poller_cfg)
+      rescue RuntimeError, ArgumentError => e
+        $LOG.error("POLLER: Error encountered while polling #{device}: #{e}")
+        metadata[:last_poll_result] = 1
+        post_devices = { device => { :metadata => metadata, :interfaces => {} } }
+        _post_data(post_devices)
+      end
 
-    # This determines which OID names will get turned into per-second averages.
-    avg_oid_regex = /octets|discards|errors|pkts/
+      InfluxDB::Logging.logger = $LOG
+      influxdb = InfluxDB::Client.new(
+        poller_cfg[:influx_db],
+        :host => poller_cfg[:influx_ip],
+        :username => poller_cfg[:influx_user],
+        :password => poller_cfg[:influx_pass],
+        :retry => 1)
 
-    # These are the OIDs that will get pulled/stored for our interfaces.
-    oid_names = Hash[
-      'if_name'        => '1.3.6.1.2.1.31.1.1.1.1',
-      'if_hc_in_octets'  => '1.3.6.1.2.1.31.1.1.1.6',
-      'if_hc_out_octets' => '1.3.6.1.2.1.31.1.1.1.10',
-      'if_hc_in_ucast_pkts' => '1.3.6.1.2.1.31.1.1.1.7',
-      'if_hc_out_ucast_pkts' => '1.3.6.1.2.1.31.1.1.1.11',
-      'if_high_speed'   => '1.3.6.1.2.1.31.1.1.1.15',
-      'if_alias'       => '1.3.6.1.2.1.31.1.1.1.18',
-      'if_mtu'         => '1.3.6.1.2.1.2.2.1.4',
-      'if_admin_status' => '1.3.6.1.2.1.2.2.1.7',
-      'if_oper_status'  => '1.3.6.1.2.1.2.2.1.8',
-      'if_in_discards'  => '1.3.6.1.2.1.2.2.1.13',
-      'if_in_errors'    => '1.3.6.1.2.1.2.2.1.14',
-      'if_out_discards' => '1.3.6.1.2.1.2.2.1.19',
-      'if_out_errors'   => '1.3.6.1.2.1.2.2.1.20'
-    ]
-    # Create the reverse hash of the OIDs above so we can easly get names from keys
-    oid_numbers = oid_names.invert
+      # TODO: Need to use stale_indexes to delete stuff
+      stale_indexes, last_values = _get_last_values(device, if_table)
 
-    # This is where we define what the averages will be named
-    avg_names = Hash[
-      'if_hc_in_octets'  => 'bps_in',
-      'if_hc_out_octets' => 'bps_out',
-      'if_in_discards'  => 'discards_in',
-      'if_in_errors'    => 'errors_in',
-      'if_out_discards' => 'discards_out',
-      'if_out_errors'   => 'errors_out',
-      'if_hc_in_ucast_pkts' => 'pps_in',
-      'if_hc_out_ucast_pkts' => 'pps_out'
-    ]
+      # Run through the hash we got from poll, processing the interesting interfaces
+      interfaces = {}
+      if_table.each do |if_index, oids|
+        # Skip if we're not interested in processing this interface
+        next unless oids['if_alias'] =~ poller_cfg[:interesting_alias]
+        interfaces[if_index] = _process_interface(device, if_index, oids, last_values, influxdb, poller_cfg)
+      end
+      $LOG.info("SNMP poll successful for #{device}: " + 
+                "#{if_table.size} interfaces polled, #{interfaces.size} processed")
 
-    begin # Start exception handling
+      # Update the application
+      metadata[:last_poll_duration] = Time.now.to_i - start_time.to_i
+      metadata[:last_poll_result] = 0
+      metadata[:last_poll_text] = ''
+      post_devices = { device => { :metadata => metadata, :interfaces => interfaces } }
 
-      pid = fork do
+      _post_data(post_devices)
 
-        if_table = {}
-        metadata = { :worker => Socket.gethostname }
-        count = nil
-        begin
-          # get SNMP data from the device
-          count, if_table = query_device(ip, poller_cfg[:snmpv2_community], oid_numbers)
-        rescue RuntimeError, ArgumentError => e
-          $LOG.error("POLLER: Error encountered while polling #{device}: #{e}")
-          metadata[:last_poll_result] = 1
-          post_devices = {
-            device => {
-              :metadata => metadata,
-              :interfaces => {},
-            },
-          }
-          post_data(post_devices)
-          abort
-        end
-
-        InfluxDB::Logging.logger = $LOG
-        influxdb = InfluxDB::Client.new(
-          poller_cfg[:influx_db],
-          :host => poller_cfg[:influx_ip],
-          :username => poller_cfg[:influx_user],
-          :password => poller_cfg[:influx_pass],
-          :retry => 1)
-
-        stale_indexes = [] # TODO: Need to use this to delete old interfaces
-
-        request = "/v1/devices?device=#{device}"
-        devices = API.get('core', request)
-        unless devices # HTTP request failed
-          $LOG.error("POLLER: HTTP request to get previous data failed: #{request}")
-          devices = {}
-        end
-        last_values = devices[device] || {}
-        last_values.each do |index,oids|
-          oids.each { |name,value| oids[name] = to_i_if_numeric(value) }
-          stale_indexes.push(index) unless if_table[index]
-        end
-
-        # Run through the hash we got from poll, processing the interesting interfaces
-        interfaces = {}
-
-        if_table.each do |if_index, oids|
-          # Skip if we're not interested in processing this interface
-          next unless oids['if_alias'] =~ poller_cfg[:interesting_alias]
-
-          interfaces[if_index] = oids.dup
-          interfaces[if_index]['if_index'] = if_index
-          interfaces[if_index]['device'] = device
-          interfaces[if_index]['last_updated'] = Time.now.to_i
-
-          # Update the last change time if these values changed.
-          %w( if_admin_status if_oper_status ).each do |oid|
-            if(!last_values[if_index] || oids[oid].to_i != last_values[if_index][oid])
-              interfaces[if_index][oid + '_time'] = Time.now.to_i
-            end
-          end
-
-          oids.each do |oid_text,value|
-            series_name = device + '.' + if_index + '.' + oid_text
-            series_data = { :value => value.to_s, :time => Time.now.to_i }
-
-            # Take the difference and average it out per second since the last poll
-            #   if this OID supposed to be averaged
-            # First make sure we have 2 data points -- if not we can't average
-            if oid_text =~ avg_oid_regex && last_values[if_index]
-              avg_series_name = device + '.' + if_index + '.' + avg_names[oid_text]
-              average = (value.to_i - last_values[if_index][oid_text].to_i) / (Time.now.to_i - last_values[if_index]['last_updated'].to_i)
-              average = average * 8 if series_name =~ /octets/
-              avg_series_data = { :value => average, :time => Time.now.to_i }
-              # Calculate utilization if we're a bps OID
-              if avg_series_name =~ /bps/ && oids['if_high_speed'].to_i != 0
-                util = '%.2f' % (average.to_f / (oids['if_high_speed'].to_i * 1000000) * 100)
-                util = 100 if util.to_f > 100
-                interfaces[if_index][avg_names[oid_text] + '_util'] = util
-              end
-              # write the average
-              unless average < 0
-                interfaces[if_index][avg_names[oid_text]] = average
-                influxdb.write_point(avg_series_name, avg_series_data)
-              end
-            end
-          end # End oids.each
-        end # End if_index.each
-
-        # Update the application
-        metadata[:last_poll_duration] = Time.now.to_i - beginning.to_i
-        metadata[:last_poll_result] = 0
-        metadata[:last_poll_text] = ''
-        post_devices = {
-          device => {
-            'metadata' => metadata,
-            'interfaces' => interfaces,
-          },
-        }
-        result = post_data(post_devices)
-        if result == 500
-          $LOG.error("POLLER: Failed to contact main instance for post " + 
-                     "(#{device}: #{count} interfaces polled, #{interfaces.keys.size} returned)")
-        else
-          $LOG.info("POLLER: Poll succeeded " + 
-                    "(#{device}: #{count} interfaces polled, #{interfaces.keys.size} returned)")
-        end
-
-      end # End fork
-
-      Process.detach(pid)
-      $LOG.info("POLLER: Forked PID #{pid} (#{device})")
-
-    rescue StandardError => error
-      $LOG.error("POLLER: #{error}")
-      raise error
-    end
+    end # End fork
+    Process.detach(pid)
+    $LOG.info("POLLER: Forked PID #{pid} (#{device})")
   end
 
-  def self.query_device(ip, community, oid_numbers)
-    SNMP::Manager.open(:host => ip, :community => community) do |session|
+  def self._process_interface(device, if_index, oids, last_values, influxdb, poller_cfg)
+    oids['if_index'] = if_index
+    oids['device'] = device
+    oids['last_updated'] = Time.now.to_i
+
+    last_oids = last_values[:interfaces][if_index]
+
+    # Update the last change time if these values changed or don't exist
+    %w( if_admin_status if_oper_status ).each do |oid|
+      if(!last_oids || oids[oid].to_i != last_oids[oid])
+        oids[oid + '_time'] = Time.now.to_i
+      end
+    end
+
+    oids.dup.each do |oid_text, value|
+      series_name = device + '.' + if_index + '.' + oid_text
+      series_data = { :value => value.to_s, :time => Time.now.to_i }
+
+      # Take the difference and average it out per second since the last poll
+      #   if this OID supposed to be averaged
+      # First make sure we have 2 data points -- if not we can't average
+      if oid_text =~ poller_cfg[:avg_oid_regex] && last_oids
+        avg_series_name = device + '.' + if_index + '.' + poller_cfg[:avg_names][oid_text]
+        average = (value.to_i - last_oids[oid_text].to_i) / (Time.now.to_i - last_oids['last_updated'].to_i)
+        average = average * 8 if series_name =~ /octets/
+        avg_series_data = { :value => average, :time => Time.now.to_i }
+        # Calculate utilization if we're a bps OID
+        if avg_series_name =~ /bps/ && oids['if_high_speed'].to_i != 0
+          util = '%.2f' % (average.to_f / (oids['if_high_speed'].to_i * 1000000) * 100)
+          util = 100 if util.to_f > 100
+          oids[poller_cfg[:avg_names][oid_text] + '_util'] = util
+        end
+        # write the average
+        unless average < 0
+          oids[poller_cfg[:avg_names][oid_text]] = average
+          influxdb.write_point(avg_series_name, avg_series_data)
+        end
+      end
+    end # End oids.each
+    return oids
+  end
+
+  def self._query_device(ip, poller_cfg)
+    SNMP::Manager.open(:host => ip, :community => poller_cfg[:snmpv2_community]) do |session|
       if_table = {}
-      count = 0
-      session.walk(oid_numbers.keys) do |row|
-        count += 1
+      session.walk(poller_cfg[:oid_numbers].keys) do |row|
         row.each do |vb|
-          oid_text = oid_numbers[vb.name.to_str.gsub(/\.[0-9]+$/,'')]
+          oid_text = poller_cfg[:oid_numbers][vb.name.to_str.gsub(/\.[0-9]+$/,'')]
           if_index = vb.name.to_str[/[0-9]+$/]
           if_table[if_index] ||= {}
           if_table[if_index][oid_text] = vb.value.to_s
         end
       end
-      return count, if_table
+      return if_table
     end
   end
 
-  def self.post_data(devices, first_try=true)
-    res = API.post('core', '/v1/devices', devices)
-    unless res # HTTP request failed
-      $LOG.error("POLLER: HTTP request to post device #{devices.keys[0]} failed")
-      # If this is the first try, retry, otherwise return 500
-      if first_try
-        $LOG.error("POLLER: Retrying post for device #{devices.keys[0]} in 5 seconds...")
-        sleep 5
-        return post_data(devices, false)
-      else
-        return 500
+  def self._get_last_values(device, if_table)
+    request = "/v1/devices?device=#{device}"
+    if devices = API.get('core', request, 'POLLER', 'previous data')
+      last_values = devices[device] || {}
+      last_values.symbolize!
+      last_values[:interfaces] ||= {}
+      stale_indexes = []
+      last_values[:interfaces].each do |index,oids|
+        oids.each { |name,value| oids[name] = to_i_if_numeric(value) }
+        stale_indexes.push(index) unless if_table[index]
       end
+    else # HTTP request failed
+      abort
     end
-    return res
+    return stale_indexes, last_values
+  end
+
+  def self._post_data(devices)
+    if API.post('core', '/v1/devices', devices, 'POLLER', 'poll results')
+      $LOG.info("POLLER: POST successful for #{devices.keys[0]}")
+    else
+      $LOG.error("POLLER: POST failed for #{devices.keys[0]}; Aborting")
+    end
+    abort
+  end
+
+  def self._poller_cfg(settings)
+    # Convert poller settings into hash with symbols as keys
+    poller_cfg = settings['poller'].dup || {}
+    poller_cfg.symbolize!
+
+    # This determines which OID names will get turned into per-second averages.
+    poller_cfg[:avg_oid_regex] = /octets|discards|errors|pkts/
+
+    # These are the OIDs that will get pulled/stored for our interfaces.
+    poller_cfg[:oid_numbers] = Hash[
+      '1.3.6.1.2.1.31.1.1.1.1'  => 'if_name',
+      '1.3.6.1.2.1.31.1.1.1.6'  => 'if_hc_in_octets',
+      '1.3.6.1.2.1.31.1.1.1.10' => 'if_hc_out_octets',
+      '1.3.6.1.2.1.31.1.1.1.7'  => 'if_hc_in_ucast_pkts',
+      '1.3.6.1.2.1.31.1.1.1.11' => 'if_hc_out_ucast_pkts',
+      '1.3.6.1.2.1.31.1.1.1.15' => 'if_high_speed',
+      '1.3.6.1.2.1.31.1.1.1.18' => 'if_alias',
+      '1.3.6.1.2.1.2.2.1.4'     => 'if_mtu',
+      '1.3.6.1.2.1.2.2.1.7'     => 'if_admin_status',
+      '1.3.6.1.2.1.2.2.1.8'     => 'if_oper_status',
+      '1.3.6.1.2.1.2.2.1.13'    => 'if_in_discards',
+      '1.3.6.1.2.1.2.2.1.14'    => 'if_in_errors',
+      '1.3.6.1.2.1.2.2.1.19'    => 'if_out_discards',
+      '1.3.6.1.2.1.2.2.1.20'    => 'if_out_errors',
+    ]
+
+    # This is where we define what the averages will be named
+    poller_cfg[:avg_names] = Hash[
+      'if_hc_in_octets'     => 'bps_in',
+      'if_hc_out_octets'    => 'bps_out',
+      'if_in_discards'      => 'discards_in',
+      'if_in_errors'        => 'errors_in',
+      'if_out_discards'     => 'discards_out',
+      'if_out_errors'       => 'errors_out',
+      'if_hc_in_ucast_pkts' => 'pps_in',
+      'if_hc_out_ucast_pkts'=> 'pps_out',
+    ]
+
+    return poller_cfg
   end
 
   def self.to_i_if_numeric(str)

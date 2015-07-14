a4adc13cf8ab7b896ebdc2b9c6cc9b84f2530640
Ian Clark <ian@f85.net>
2015-07-14 16:52:27 -0700

    Fix for null descriptions in DescriptionChangeEvent

--- lib/events/description_change_event.rb
+++ lib/events/description_change_event.rb
@@ -15,8 +15,8 @@ class DescriptionChangeEvent < ComponentEvent
       device: device, hw_type: hw_type, index: index,
       time: time, comp_id: comp_id, subtype: self.class.name
     )
-    @old = old
-    @new = new
+    @old = old.empty? ? nil : old
+    @new = new.empty? ? nil : new
   end
 
 
